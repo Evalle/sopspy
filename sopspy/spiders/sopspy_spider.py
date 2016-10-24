8fe5f08e6f4fa0c59c7cefefacd4107402fc39b8
evalle <shmarnev@gmail.com>
2016-10-24 22:54:03 +0200

    Add giignore + change sopspy spider

--- sopspy/spiders/sopspy_spider.py
+++ sopspy/spiders/sopspy_spider.py
@@ -3,18 +3,13 @@ import scrapy
 
 class SopcastSpider(scrapy.Spider):
     name = "sopspy"
-
-    def start_requests(self):
-        urls = [
-            'http://livetv.sx/team/1_363_446_manchester_city/broadcasts/'
-        ]
-
-        for link in urls:
-            yield scrapy.Request(url=url, callback=self.parse)
+    urls = [
+        'http://livetv.sx/allupcomingsports/1/'
+    ]
 
     def parse(self, response):
-        page = response.url.split("/")[-2]
-        filename = 'sops-%s.html' % page
+        for href in response.xpath('//a/@href').extract():
+            yield scrapy.Request(url)
         with open(filename, 'wb') as f:
             f.write(response.body)
-        self.log('Saved file %s' % filename)
+            self.log('Saved file %s' % filename)
